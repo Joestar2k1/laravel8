688999d3dc3680429734cf3de12192e49abb76b2
HoTuan0913 <0306191384@caothang.edu.vn>
2022-01-19 15:58:24 +0700

    add-view

---
+++ resources/views/admin/app/partials/nav-bar.blade.php
@@ -0,0 +1,48 @@
+ <!-- Sidebar menu-->
+ <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
+ <aside class="app-sidebar">
+   <div class="app-sidebar__user"><img class="app-sidebar__user-avatar" src="" alt="User Image">
+     <div>
+       <p class="app-sidebar__user-name">John Doe</p>
+       <p class="app-sidebar__user-designation">Frontend Developer</p>
+     </div>
+   </div>
+   <ul class="app-menu">
+     <li><a class="app-menu__item active" href="index-2.html"><i class="app-menu__icon fas fa-home"></i><span class="app-menu__label">Dashboard</span></a></li>
+     <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fas fa-laptop"></i><span class="app-menu__label">Bảng</span><i class="treeview-indicator fas fa-angle-right"></i></a>
+       <ul class="treeview-menu">
+         <li><a class="treeview-item" href="bootstrap-components.html"><i class="icon fa fa-circle-o"></i> Sản phẩm</a></li>
+         <li><a class="treeview-item" href="https://fontawesome.com/v4.7.0/icons/" target="_blank" rel="noopener"><i class="icon fas fa-circle-o"></i> Hóa đơn</a></li>
+         <li><a class="treeview-item" href="ui-cards.html"><i class="icon fa fa-circle-o"></i>Tài khoản</a></li>
+         <li><a class="treeview-item" href="widgets.html"><i class="icon fa fa-circle-o"></i></a></li>
+       </ul>
+     </li>
+     <!-- <li><a class="app-menu__item" href="charts.html"><i class="app-menu__icon fas fa-pie-chart"></i><span class="app-menu__label">Charts</span></a></li> -->
+     <!-- <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fas fa-edit"></i><span class="app-menu__label">Forms</span><i class="treeview-indicator fas fa-angle-right"></i></a>
+       <ul class="treeview-menu">
+         <li><a class="treeview-item" href="form-components.html"><i class="icon fas fa-circle"></i> Form Components</a></li>
+         <li><a class="treeview-item" href="form-custom.html"><i class="icon fas fa-circle"></i> Custom Components</a></li>
+         <li><a class="treeview-item" href="form-samples.html"><i class="icon fas fa-circle"></i> Form Samples</a></li>
+         <li><a class="treeview-item" href="form-notifications.html"><i class="icon fas fa-circle"></i> Form Notifications</a></li>
+       </ul>
+     </li> -->
+     <!-- <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fas fa-th-list"></i><span class="app-menu__label">Tables</span><i class="treeview-indicator fas fa-angle-right"></i></a>
+       <ul class="treeview-menu">
+         <li><a class="treeview-item" href="table-basic.html"><i class="icon fas fa-circle"></i> Basic Tables</a></li>
+         <li><a class="treeview-item" href="table-data-table.html"><i class="icon fas fa-circle"></i> Data Tables</a></li>
+       </ul>
+     </li> -->
+     <!-- <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fas fa-file-text"></i><span class="app-menu__label">Pages</span><i class="treeview-indicator fas fa-angle-right"></i></a>
+       <ul class="treeview-menu">
+         <li><a class="treeview-item" href="blank-page.html"><i class="icon fas fa-circle"></i> Blank Page</a></li>
+         <li><a class="treeview-item" href="page-login.html"><i class="icon fas fa-circle"></i> Login Page</a></li>
+         <li><a class="treeview-item" href="page-lockscreen.html"><i class="icon fas fa-circle"></i> Lockscreen Page</a></li>
+         <li><a class="treeview-item" href="page-user.html"><i class="icon fas fa-circle"></i> User Page</a></li>
+         <li><a class="treeview-item" href="page-invoice.html"><i class="icon fas fa-circle"></i> Invoice Page</a></li>
+         <li><a class="treeview-item" href="page-calendar.html"><i class="icon fas fa-circle"></i> Calendar Page</a></li>
+         <li><a class="treeview-item" href="page-mailbox.html"><i class="icon fas fa-circle"></i> Mailbox</a></li>
+         <li><a class="treeview-item" href="page-error.html"><i class="icon fas fa-circle"></i> Error Page</a></li>
+       </ul>
+     </li> -->
+   </ul>
+ </aside>
