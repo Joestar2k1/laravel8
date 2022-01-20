5e91195078a245db085118886db742c0be5931ae
Joestar2k1 <iloverussian2311@gmail.com>
2022-01-20 22:09:54 +0700

    fix

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -1,88 +1,6 @@
 @extends('admin.app')
 @section('title') Admin-product @endsection
 @section('content')
-<<<<<<< HEAD
-    <div class="page-wrapper">
-
-        <div class="page-breadcrumb bg-white">
-            <div class="row align-items-center">
-                <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                    <span class="page-title">Sản phẩm<a href="{{ route('admin.products.create') }}"
-                            class="btn btn-success">Create
-                            new</a></span>
-                </div>
-            </div>
-            <!-- /.col-lg-12 -->
-        </div>
-        <div class="container-fluid">
-            <div class="container mt-3">
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>
-                                Mã sản phẩm
-                            </th>
-                            <th>
-                                Tên sản phẩm
-                            </th>
-                            <th>
-                                Số lượng TK
-                            </th>
-                            <th>
-                                Giá
-                            </th>
-                            <th>
-                                Mô tả
-                            </th>
-                            <th>
-                                Ảnh minh họa
-                            </th>
-                            <th>
-                                Loại
-                            </th>
-                            <th>
-                                Đơn vị tính
-                            </th>
-                            <th>
-                                Trạng thái
-                            </th>
-                            <th>
-                                Sửa
-                            </th>
-                            <th>
-                                Xóa
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($data as $item)
-                            <tr>
-                                <td> {{ $item->sku }}</td>
-                                <td> {{ $item->name }}</td>
-                                <td> {{ $item->stock }}</td>
-                                <td> {{ $item->price }}</td>
-                                <td> {{ $item->description }}</td>
-
-                                <td> <img src="{{ asset('backend/assets/img/products/' . $item->image) }}"
-                                        class="rounded" alt="Ảnh" width="70" height="70"> </td>
-                                <td> {{ $item->type }}</td>
-                                <td> {{ $item->unit }}</td>
-                                <td> {{ $item->status }}</td>
-                                <td>
-                                    <a class="btn btn-success">Edit</a>
-                                </td>
-                                <td>
-                                    <a class="btn btn-danger">Delete</a>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-                {{ $data->links() }}
-            </div>
-        </div>
-    </div>
-=======
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
@@ -172,6 +90,5 @@
     </div>
 </div>
 
->>>>>>> e0b6588f1c82003be5eda2c1fd2cd9cf038b75aa
 
 @endsection
