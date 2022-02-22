11305b2b14f234cec380eacae101bd4bd2e04ec4
Joestar2k1 <iloverussian2311@gmail.com>
2022-02-22 11:26:53 +0700

    Controller for Blogs

--- app/Http/Controllers/Admin/BlogController.php
+++ app/Http/Controllers/Admin/BlogController.php
@@ -33,4 +33,45 @@ class BlogController extends Controller
       ]);
       return redirect()->route('admin.blogs.index');
   }
+
+  public function FormCreateBlog(){
+        return view ('admin.blogs.create');
+  }
+  public function CreateBlog(Request $request){
+    $countPrd = DB::table('blogs')->count()+1;
+    $file_name='';
+    if($request->has('image')){
+        $file= $request->image;
+        $ext = $request->image->extension();//lấy đuôi file png||jpg
+        $file_name = Date('Ymd').'-'.'blog'.$countPrd.'.'.$ext;
+        $file->move(public_path('backend/assets/img/latest-news'),$file_name);
+    }
+    DB::table('blogs')->insert([
+        'title' => $request->title,
+        'content' => $request->content,
+        'image' => $file_name,
+        'postDated' => Date('Y-m-d'),
+        'employeeID' => Session::get('emp')->id,
+        'status' => 1,
+    ]);
+    $blogs = DB::table('blogs')
+    ->join('employees','blogs.employeeID','employees.id')
+    ->select('blogs.*')
+    ->orderBy('postDated','desc')
+    ->paginate(6);
+   
+    return view('admin.blogs.index',compact('blogs'));
+  }
+  public function DeleteBlog($id){
+    $blogs = DB::table('blogs')
+    ->where('id',$id)
+    ->delete();
+    $blogs = DB::table('blogs')
+    ->join('employees','blogs.employeeID','employees.id')
+    ->select('blogs.*')
+    ->orderBy('postDated','desc')
+    ->paginate(6);
+   
+    return view('admin.blogs.index',compact('blogs'));
+  }
 }
